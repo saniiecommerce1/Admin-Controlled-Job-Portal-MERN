c257041b005234a19cf228bbab638baa60b2a602
saniiecommerce1 <sanii.ecommerce@gmail.com>
2026-04-19 22:15:05 +0500

    docker file created

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ import companyRoute from "./route/company.route.js";
 import userRoute from "./route/user.route.js";
 import jobRoute from "./route/job.route.js";
 import applicationRoute from "./route/application.route.js";
+import path from "path";
+
+
+
 
 import dotenv from "dotenv";
 dotenv.config()
@@ -15,37 +19,37 @@ dotenv.config()
  app.use(express.json())
  app.use(cookieParser())
 
+const __dirname = path.resolve();
+
+
+//  app.use(cors({
+//     origin : process.env.CLIENT_URL,
+//     credentials: true    
+//  }))
+
+//FE + BE same server 
+//credentials true is must for cookie to work in FE + BE same server
 
  app.use(cors({
-    origin : process.env.CLIENT_URL,
-    credentials: true    
- }))
+    origin : "*",
+     credentials: true
+ }));
 
-app.get('/cookie' , (req, res)=>{
-   res.cookie("JWT-TOKEN", 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTM0NmVkOTVjY2RmY2JmMDM5ZGExYjciLCJpYXQiOjE3NjY4NDU0NzgsImV4cCI6MTc2NzQ1MDI3OH0.OlEVdplIDeC8JMbdLfrszBw5CZ8yOHHwGo53SystwFo; Path=/; HttpOnly; Expires=Sat, 03 Jan 2026 14:24:38 GMT;', {
-            maxAge: 7 * 24 * 60 * 60 * 1000, // ms in a week           
-            httpOnly: true, // prevent XSS attacks: cross-site scripting document.cookie cannot access jwt cookie
-            sameSite: "strict", // CSRF attacks customer site request forgery
-            secure: process.env.NODE_ENV === "production" ? true : false //support https only in production
-   }).status(201).json(
-            {
-                message: "Login Successfully",                
-                success: true
-            })
-
-
-
-
-})
 
  app.use('/api/user' , userRoute)
  app.use('/api/company' , companyRoute)
  app.use('/api/job' , jobRoute)
  app.use('/api/application' , applicationRoute)
 
+ // serve frontend
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
- app.listen(process.env.PORT, ()=>{
+//must add 0.0.0.0 route for Docker to work
+ app.listen(process.env.PORT, '0.0.0.0', ()=>{
     console.log(`Server is connected on PORT : ${process.env.PORT}`)
     connectDB()
     
